fb5d7e2a668a864d38ca78e9337e9022b81a3e66
Taira manuel desire <desiretaira@gmail.com>
2018-08-19 21:31:47 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
     require 'vendor\autoload.php';
 
-    $bdd = new \app\database\Database();
-    $req = $bdd->getPDO()->query('SELECT * FROM users');
-    $datas = $req->fetchAll(PDO::FETCH_OBJ);
-    foreach ($datas as $data) {
-    var_dump($data);
-    }
-    return $datas;
-
