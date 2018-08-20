d2129a87afe0f32404cd9f4cb6997c61c877d8ba
Taira manuel desire <desiretaira@gmail.com>
2018-08-19 22:22:28 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
     require 'vendor\autoload.php';
 
-<<<<<<< HEAD
+
    $app = new \app\controller\Controller();
    $app->run();
 
-=======
->>>>>>> fb5d7e2a668a864d38ca78e9337e9022b81a3e66
